db7564dedc7ce163c6d86dbec3039216ae90e078
ezDiGiDigital <104407742+ezDiGi@users.noreply.github.com>
2023-01-31 22:11:19 +0700

    Update configme.sh

--- configme.sh
+++ configme.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-read -p "Vui lòng nhập node ID :" aiko_node_id
-      [ -z "${aiko_node_id}" ]
-      echo -e "${green}Node ID của bạn đặt là: ${aiko_node_id}${plain}"
-      echo -e "-------------------------"
-
-      wget https://raw.githubusercontent.com/ezDiGi/config/main/configme.yml -O /etc/XrayR/config.yml
-      sed -i "s/NodeID:.*/NodeID: ${aiko_node_id}/g" /etc/XrayR/config.yml
+wget https://raw.githubusercontent.com/ezDiGi/NTQ/main/hk.yml -O /etc/XrayR/aiko.yml
