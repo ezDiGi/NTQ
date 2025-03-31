ecd865e81ee87bae6afec68b547ea5b1ffea1466
ezDiGiDigital <104407742+ezDiGi@users.noreply.github.com>
2025-03-31 21:13:18 +0700

    Update proxy.sh

--- proxy.sh
+++ proxy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-wget https://ezdigi.vn/AikoR/config/squid.conf -O /etc/squid/squid.conf
+wget https://raw.githubusercontent.com/ezDiGi/NTQ/refs/heads/main/squid.conf -O /etc/squid/squid.conf
